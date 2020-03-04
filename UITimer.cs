4cc8192d2a86e1cf52a7d6a3765a0ccc25b94851
ThisIsFix <50284031+ThisIsFix@users.noreply.github.com>
2020-03-04 10:41:45 -0500

    Update UITimer.cs

--- UITimer.cs
+++ UITimer.cs
@@ -12,9 +12,9 @@ public class UITimer : MonoBehaviour
  	if(playing == true){
   
 	  Timer += Time.deltaTime;
-	  int minutes = Mathf.FloorToInt(Timer / 60F);
-	  int seconds = Mathf.FloorToInt(Timer % 60F);
-	  int milliseconds = Mathf.FloorToInt((Timer * 100F) % 100F);
+	  int minutes = Mathf.FloorToInt(Timer / 60f);
+	  int seconds = Mathf.FloorToInt(Timer % 60f);
+	  int milliseconds = Mathf.FloorToInt((Timer * 100f) % 100f);
 	  TimerText.text = minutes.ToString ("00") + ":" + seconds.ToString ("00") + ":" + milliseconds.ToString("00");
 	}
 
